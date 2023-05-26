4abc8dbb21a636f1914d50becf52fffb56634975
Ivaylo Stavrev <114228905+Stavrev0102@users.noreply.github.com>
2023-05-26 19:36:54 +0300

    Add static asset resolving

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
-const handlebars = require('express-handlebars')
+const handlebars = require('express-handlebars');
+const path = require('path');
 const PORT = 3000;
+
+//Express config(public folder)
+app.use(express.static(path.resolve(__dirname,'public')))
 
 //Set up handlebars
 app.engine('hbs',handlebars.engine({
