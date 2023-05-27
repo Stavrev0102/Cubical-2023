a94911a74c048e4f82930a774496a776ce6a5137
Ivaylo Stavrev <114228905+Stavrev0102@users.noreply.github.com>
2023-05-27 21:16:04 +0300

    Add routes

--- src/index.js
+++ src/index.js
@@ -3,17 +3,12 @@ const app = express();
 const PORT = 3000;
 const handlebarsConfig = require('./config/handlebarsConfig');
 const expressConfig = require('./config/expressConfig');
-const homeController = require('./controllers/homeController');
-const cubeController = require('./controllers/cubeController');
+const routes = require('./routes')
 
 expressConfig(app);
 handlebarsConfig(app);
+app.use(routes);
 
-app.use(homeController);
-app.use('/cubes',cubeController);
-app.get('*',(req,res) => {
-res.redirect('/404')
-});
 
 
 app.listen(PORT,()=>{console.log(`Server is listening on port ${PORT}...`)});
