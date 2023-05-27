215d6cac8b9ac62daf8382a81c5747c310f554ee
Ivaylo Stavrev <114228905+Stavrev0102@users.noreply.github.com>
2023-05-27 18:01:03 +0300

    Add cube controller.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ const PORT = 3000;
 const handlebarsConfig = require('./config/handlebarsConfig');
 const expressConfig = require('./config/expressConfig');
 const homeController = require('./controllers/homeController');
+const cubeController = require('./controllers/cubeController');
 
 expressConfig(app);
 handlebarsConfig(app);
 
-app.use(homeController)
+app.use(homeController);
+app.use('/cubes',cubeController);
 
 
 app.listen(PORT,()=>{console.log(`Server is listening on port ${PORT}...`)});
