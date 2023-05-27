e0a07db4c9fd173901dd1426ac8b9422f9828469
Ivaylo Stavrev <114228905+Stavrev0102@users.noreply.github.com>
2023-05-27 20:20:59 +0300

    Add cube details page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -18,10 +18,15 @@ cubeManager.create({ name,
     description,
     imageUrl,
     difficultyLevel:Number(difficultyLevel),
-})
+});
 
 
 res.redirect('/');
 });
+
+router.get('/:cubeId/details',(req,res) => {
+    const cube = cubeManager.getOne(req.params.cubeId)
+    res.render('details',{cube})
+    });
 
 module.exports = router;
--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,9 +1,21 @@
 const uniqid = require('uniqid')
-const cubes = [];
+const cubes = [
+    {
+        id: '1ojxg1xp8li66ld8y',
+    name: 'test1',
+    description: 'very good cube for kids',
+    imageUrl: 'https://cdn.shopify.com/s/files/1/0482/9177/4628/products/o3rmamdfu2kartbxpul9.jpg?v=1681761411',
+    difficultyLevel: 3
+    }
+];
 
 exports.getAll = () => {
     return cubes.slice()
-}
+};
+
+exports.getOne = (cubeId) => {
+return cubes.find(x => x.id == cubeId)
+};
 
 exports.create = (cubeData) => {
     const newCube = {
