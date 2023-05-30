4863f8ddb823bb8b2c351a9bc5121836a7d5d3fb
Ivaylo Stavrev <114228905+Stavrev0102@users.noreply.github.com>
2023-05-30 20:29:41 +0300

    Show remaining only

--- src/managers/accessoryManager.js
+++ src/managers/accessoryManager.js
@@ -5,3 +5,5 @@ const Accessory = require('../models/Accessory');
 exports.create = (accessoryData) => Accessory.create(accessoryData)
 
 exports.getAll = () => Accessory.find();
+
+exports.getOthers = (accessoryIds) => Accessory.find({_id: { $nin:accessoryIds}})
