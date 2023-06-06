d5a72e8ffd040dd62d3ed9ec767eb372cf2ae72e
Ivaylo Stavrev <114228905+Stavrev0102@users.noreply.github.com>
2023-06-06 19:30:34 +0300

    Add delete cube page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -5,7 +5,7 @@ const accessoryManager = require('../managers/accessoryManager');
 
 router.get('/create',(req,res) => {
 
-res.render('create')
+res.render('cube/create')
 });
 
 router.post('/create',async (req,res) => {
@@ -31,7 +31,7 @@ router.get('/:cubeId/details', async(req,res) => {
     if(!cube){
         return res.redirect('/404')
     }
-    res.render('details',{cube})
+    res.render('cube/details',{cube})
     });
 
 router.get('/:cubeId/attach-accessories', async(req,res) => {
@@ -50,5 +50,11 @@ router.post('/:cubeId/attach-accessories',async (req,res) => {
     await cubeManager.attachAccessory(cubeId,accessory);
     res.redirect(`/cubes/${cubeId}/details`)
 });
+
+router.get('/:cubeId/delete',async(req,res) => {
+    const cube = await cubeManager.getOne(req.params.cubeId).lean();
+    console.log(cube)
+    res.render('cube/delete',{ cube })
+})
 
 module.exports = router;
